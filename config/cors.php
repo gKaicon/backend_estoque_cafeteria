3894b60d879838275f639ef156a0b7cb0b2c8a4e
Gabriel Káicon <gkaicon2@gmail.com>
2025-07-31 16:54:13 -0300

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://estoque-cafeteria.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://estoque-cafeteria.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
