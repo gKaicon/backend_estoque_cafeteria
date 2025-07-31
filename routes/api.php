94406b2bbd819cdde914395394fb4c29fa6ae12f
Gabriel Káicon <gkaicon2@gmail.com>
2025-07-31 01:26:06 -0300

    innitial commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellController;
+use App\Http\Controllers\BuyController;
 
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh'); 
+    Route::get('me', 'me'); 
+});
+
+Route::apiResource('products', ProductController::class);
+Route::apiResource('sells', SellController::class);
+Route::apiResource('buys', BuyController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
-use App\Http\Controllers\ProdutoController;
-use App\Http\Controllers\UserController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,19 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-
-Route::get('/login', [UserController::class, 'digitarLogin'])->name('login');
-Route::post('/login', [UserController::class, 'login']);
-Route::get('/logout', [UserController::class, 'logout']);
-
-Route::get('/produtos', [ProdutoController::class, 'exibirProdutos']);
-Route::get('/produtos/criar', [ProdutoController::class, 'criarProduto'])->middleware('auth');
-Route::post('/produtos/armazenar', [ProdutoController::class, 'armazenarProduto'])->middleware('auth');
-Route::post('/produtos/editar', [ProdutoController::class, 'editarProduto'])->middleware('auth');
-Route::post('/produtos/atualizar', [ProdutoController::class, 'atualizarProduto'])->middleware('auth');
-Route::post('/produtos/deletar', [ProdutoController::class, 'deletarProduto'])->middleware('auth');
-
-
-
